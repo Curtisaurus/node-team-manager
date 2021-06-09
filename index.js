af5f45ffe44134158b953de8915c4274eb453781
Curtisaurus <ccdrullinger@gmail.com>
2021-06-08 23:54:31 -0400

    created index.js and declared required modules

---
+++ index.js
@@ -0,0 +1,6 @@
+// declaring modules
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
